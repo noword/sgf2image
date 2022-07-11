b5643c443a4b44ad00885ee4de21efaeb1fe17f3
noword <noword.forever@gmail.com>
2022-07-11 13:13:53 +0800

    can gen part rectangle image

--- sgf2img.py
+++ sgf2img.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     parser.add_argument('--theme', action='store', default='real-stones', help='default theme is real-stones')
     parser.add_argument('--disable_coordinates', action='store_true', default=False)
     parser.add_argument('--list_themes', action='store_true', default=False, help='list all available themes')
+    parser.add_argument('--part_rect', action='store', nargs='?', help='left,top,bottom,right')
     args = parser.parse_args()
 
     themes = GetAllThemes()
@@ -23,5 +24,13 @@ if __name__ == '__main__':
         parser.print_help()
     else:
         theme = themes[args.theme]
+        part_rect = args.part_rect
+        if part_rect is not None:
+            part_rect = [int(x) for x in part_rect.split(',')]
         gig = GameImageGenerator(theme, not args.disable_coordinates)
-        gig.get_game_image(args.sgf_name, args.img_size, args.start_number, args.start, args.end).save(args.image_name)
+        gig.get_game_image(args.sgf_name,
+                           args.img_size,
+                           args.start_number,
+                           args.start,
+                           args.end,
+                           part_rect=part_rect).save(args.image_name)
